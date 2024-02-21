a9fca943af61002367f572a433053def0c7758fe
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-02-21 18:01:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const buttonSubmit = document.querySelector("#button");
 
 buttonSubmit.addEventListener("click", function(event) {
     event.preventDefault();
-   let pis = (document.getElementById("pis_cofins").value);
+    let pis = (document.getElementById("pis_cofins").value);
     let icms = (document.getElementById("icms").value);
     let precoNet = (document.getElementById("preco_net").value);
     let taxaUsd = (document.getElementById("taxa_usd").value);
@@ -24,11 +24,13 @@ const impostoTotal = parseFloat(icms)+parseFloat(pis);/* somando icms + pis e co
 const liquido = parseFloat(porcent)-parseFloat(impostoTotal); // imposto total - 100%//
 const precoInput = parseFloat(precoNet)/parseFloat(liquido)*parseFloat(porcent); //recebe o preço net e divide pelo liqudo//
 const precoArredondado = (precoInput.toFixed(4)); //precisei utiliar o toFixed para diminuir as casas decimais//
-converDolar = precoArredondado * taxaUsd;
+const converDolar = precoArredondado * taxaUsd;
+const dolarArredondado = (converDolar.toFixed(4));
 
-document.querySelector(".valor_imposto").innerHTML = `${impostoTotal}`;
-document.querySelector(".valor_liquido").innerHTML = `${liquido}`;
-document.querySelector(".valor_bruto").innerHTML = `${precoArredondado}`;
-document.querySelector(".valor_brutoReal").innerHTML = `${converDolar}`;
 
+//console.log(impostoTotal,liquido,precoArredondado,converDolar);
+document.querySelector(".valor_imposto").innerHTML = `<strong>Imposto Total:</strong> ${impostoTotal}%`;
+document.querySelector(".valor_liquido").innerHTML = `<strong>Valor Líquido:</strong> ${liquido}%`;
+document.querySelector(".valor_bruto").innerHTML = `<strong>Valor Bruto:</strong> ${precoArredondado}`;
+document.querySelector(".valor_brutoReal").innerHTML = ` <strong>Valor Bruto Real: </strong> R$ ${dolarArredondado}`;
 })
